aa7edab10d62f9e50275f620e092bca2eb5293f5
Bruno Freire <brunofreire1717@gmail.com>
2023-05-17 15:46:25 -0500

    conectedBackEnd

--- src/components/auth/register_form/index.jsx
+++ src/components/auth/register_form/index.jsx
@@ -1,13 +1,51 @@
+import { NavLink, redirect, useNavigate } from 'react-router-dom'
 import './index.scss'
+import { useState } from 'react'
+import UsersServices from '../../../services/users'
+
+const initialState = {
+  name: '', email: '', password: ''
+}
 
 const RegisterForm = () => {
-    return (<form>
+    const [fields, setFields] = useState(initialState)
+    const [redirectToLogin, setRedirectToLogin] = useState(false)
+    const [error, setError] = useState(false)
+
+    const handleFieldsChange = event => {
+      setFields({
+        ...fields, [event.currentTarget.name]: event.currentTarget.value
+      })
+    }
+    const navigate = useNavigate()
+
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      try {
+        const user = await UsersServices.register({name: fields.name, email: fields.email, password: fields.password})
+        setRedirectToLogin(true)
+      } catch (error) {
+        setError(true)
+      }
+    }
+
+    if(redirectToLogin){
+      return navigate('/login')
+    }
+
+    return (
+    <form onSubmit={handleSubmit}>
       <label htmlFor="name">Name:</label>
-      <input type="text" name="name" id="name" />
+      <input type="text" name="name" id="name" value={fields.name} required onChange={handleFieldsChange}/>
       <label htmlFor="email">Email:</label>
-      <input type="text" name="email" id="email"/>
-      <label htmlFor="password">password:</label>
-      <input type="password" name="password" id="password"/>
+      <input type="text" name="email" id="email" value={fields.email} required onChange={handleFieldsChange}/>
+      <label htmlFor="password">Password:</label>
+      <input type="password" name="password" id="password" value={fields.password} required onChange={handleFieldsChange}/>
+      {error && <p style={{color: 'rgb(255, 0, 0)', margin: '0'}}>Invalid password or email</p>}
+      <div className="btnLoginOrRegister">
+        <NavLink to='/login'><p className="login text">Login or</p></NavLink>
+        <button type='submit' className="register text">Register</button>
+      </div>
     </form>
     )
 }
