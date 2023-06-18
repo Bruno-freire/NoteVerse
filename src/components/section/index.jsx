f04e7e6f3ad30a5e5f4aad548959dfd82c989f1c
Bruno Freire <brunofreire1717@gmail.com>
2023-06-18 13:01:50 -0500

    addText

--- src/components/section/index.jsx
+++ src/components/section/index.jsx
@@ -7,8 +7,9 @@ const Section = () => (
     <div className="home">
       <div className="columnLeft">
         <h1>Create notes easily<br/> and access when you wants<br/> on the cloud</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi corporis dolor modi sunt, deserunt accusamus.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos, architecto neque repudiandae modi doloremque consectetur.</p>
+        <p>Take control of your notes with JavaScript Notes: Create, access, and organize effortlessly on the cloud.</p>
+        <p>With JavaScript Notes, you can say goodbye to the limitations of traditional note taking methods. Say goodbye to lost notebooks and forgotten doodles. </p>
+        <p>Our innovative cloud-based platform allows you to easily capture your thoughts, ideas and important information, ensuring they are securely stored and readily available whenever and wherever you need them.</p>
         <NavLink to='/register'>Register for free Now</NavLink>
       </div>
       <div className="columnRight">
