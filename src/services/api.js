27c278353780b7bddf7f6ebef2b6c4102fc4dfa1
bruno <brunofreire1717@gmail.com>
2025-02-02 14:32:47 -0500

    feat: add .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const Api = axios.create({baseURL: 'https://apirestfullfornoteverseproject.onrender.com'});
+const Api = axios.create({baseURL: import.meta.env.VITE_URL_API_BACKEND || "http://localhost:3000"});
 
 export default Api;
